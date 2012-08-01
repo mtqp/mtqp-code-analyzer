4ec6dd8fae08002e011e7487650a308badd20f27
marian.sabianaa <marian.sabianaa@50e44dfa-913a-cf03-778a-4e8969c24434>
2012-08-01 17:56:47 +0000

    continuacion con GraphicObject

---
+++ trunk/CodeAnalyzer/Analyzer/Enums/GraphicPropertyEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeAnalyzer.Analyzer
+{
+    public enum GraphicPropertyEnum
+    {
+        Size,
+        XY,
+        TabIndex
+    }
+}
